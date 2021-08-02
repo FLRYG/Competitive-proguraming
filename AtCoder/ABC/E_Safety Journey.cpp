b0eaa1e8b37081ac15d864140cfd4f038e9a9ac1
FLRYG <flryg1113169@gmail.com>
2021-08-02 23:23:55 +0900

    2021.08.02

--- AtCoder/ABC/E_Safety Journey.cpp
+++ AtCoder/ABC/E_Safety Journey.cpp
@@ -27,42 +27,106 @@ typedef long double ld;
 double const PI=3.141592653589793;
 int const INF=1001001001;
 ll const LINF=1001001001001001001;
-ll const MOD=1000000007;
+ll const MOD=998244353;
 
-ll mpow(ll x,ll n){
-    if(n==0) return 1;
-    else if(n%2) return x*mpow(x,n-1)%MOD;
-    return mpow(x*x%MOD,n/2)%MOD;
-}
-
-ll N,M,K;
+int N,M,K;
 
 int main(){
     cin>>N>>M>>K;
-    vector<vector<ll>> G(N);
+    vector<vector<int>> G(N+1);
     rep(i,M){
-        ll u,v;
+        int u,v;
         cin>>u>>v;
-        u--, v--;
         G[u].push_back(v);
         G[v].push_back(u);
     }
-    rep(i,N) G[i].push_back(i);
-    rep(i,N) sort(G[i].begin(),G[i].end());
+    repn(i,N) G[i].push_back(i);
 
-    vector<ll> cnt(N,0);
-    repr(e,G[0]) cnt[e]=1;
-    rep(i,K-1){
-        rep(j,N) repr(e,G[j]){
-            cnt[e]+=cnt[j];
-            cnt[e]%=MOD;
+    vector<ll> sum(K+1,0);
+    vector<vector<ll>> dp(K+1,vector<ll>(N+1,0));
+    sum[0]=1;
+    dp[0][1]=1;
+    repn(i,K) repn(j,N){
+        dp[i][j]=sum[i-1];
+        repr(e,G[j]){
+            dp[i][j]-=dp[i-1][e];
+            dp[i][j]%=MOD;
         }
+        sum[i]+=dp[i][j];
+        sum[i]%=MOD;
     }
+    if(dp[K][1]<0) dp[K][1]+=MOD;
 
-    ll ans=mpow(2,MOD)*K%MOD*(K-1)%MOD*(N-1)%MOD;
-    ans-=cnt[0];
-
-    cout<<
+    cout<<dp[K][1]<<endl;
     
     return 0;
 }
+
+// #include <iostream>
+// #include <stdio.h>
+// #include <algorithm>
+// #include <cmath>
+// #include <string>
+// #include <vector>
+// #include <list>
+// #include <iomanip>
+// #include <stack>
+// #include <queue>
+// #include <deque>
+// #include <set>
+// #include <map>
+// #include <unordered_map>
+// #include <bitset>
+// #include <chrono>
+// #include <random>
+// #define rep(i,n) for(int i=0;i<n;i++)
+// #define repn(i,n) for(int i=1;i<=n;i++)
+// #define repr(e,x) for(auto& e:x)
+// using namespace std;
+// typedef long long ll;
+// typedef long double ld;
+// // typedef pair<int,int> P;
+// // typedef pair<int,P> IP;
+// // typedef pair<P,P> PP;
+// double const PI=3.141592653589793;
+// int const INF=1001001001;
+// ll const LINF=1001001001001001001;
+// ll const MOD=1000000007;
+
+// ll mpow(ll x,ll n){
+//     if(n==0) return 1;
+//     else if(n%2) return x*mpow(x,n-1)%MOD;
+//     return mpow(x*x%MOD,n/2)%MOD;
+// }
+
+// ll N,M,K;
+
+// int main(){
+//     cin>>N>>M>>K;
+//     vector<vector<ll>> G(N);
+//     rep(i,M){
+//         ll u,v;
+//         cin>>u>>v;
+//         u--, v--;
+//         G[u].push_back(v);
+//         G[v].push_back(u);
+//     }
+//     rep(i,N) G[i].push_back(i);
+//     rep(i,N) sort(G[i].begin(),G[i].end());
+
+//     vector<ll> cnt(N,0);
+//     repr(e,G[0]) cnt[e]=1;
+//     rep(i,K-1){
+//         rep(j,N) repr(e,G[j]){
+//             cnt[e]+=cnt[j];
+//             cnt[e]%=MOD;
+//         }
+//     }
+
+//     ll ans=mpow(2,MOD)*K%MOD*(K-1)%MOD*(N-1)%MOD;
+//     ans-=cnt[0];
+
+//     cout<<
+    
+//     return 0;
+// }
